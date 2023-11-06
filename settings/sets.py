114bc1a43a8cee0877a654deeaec520dc75eb5d9
AnastasiiaOkhremchukQA26 <nastia.o0403@gmail.com>
2023-11-06 16:23:57 +0000

    add BasePage

---
+++ settings/sets.py
@@ -0,0 +1,5 @@
+PROD_SERVER = "https://oriontoys.shop/"
+TEST_EMAIL = "test.mail@mail.com"
+PASSWORD = "Qwerty54321"
+IMPLICITLY_WAIT = 10 #Неявні очікування
+EXPLICIT_WAIT = 10 #Явні очікування
---
+++ tests/test_main_page.py
@@ -0,0 +1,15 @@
+import pytest
+from ..pages.base_page import BasePage
+from ..settings import sets
+
+@pytest.mark.smoke
+@pytest.mark.regression
+@pytest.mark.main_page
+class TestMainPage:
+
+    def setup_method(self):
+        pass
+
+    def test_get_main_page(self, browser):
+        page = BasePage(browser, sets.PROD_SERVER)
+        page.open()
