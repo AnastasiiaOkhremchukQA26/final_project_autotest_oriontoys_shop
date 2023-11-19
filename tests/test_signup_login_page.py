1db9784add8da73ce5627ef3a366e77a1b4b792f
AnastasiiaOkhremchukQA26 <nastia.o0403@gmail.com>
2023-11-19 20:20:05 +0000

    init

--- tests/test_signup_login_page.py
+++ tests/test_signup_login_page.py
@@ -15,6 +15,10 @@ class TestSignupLoginLogautPage:
         hash_name = "%032x" % random.getrandbits(128)
         self.email_for_signup = f"{hash_name}@mail.com"
         self.password_for_signup = "123654789"
+        self.name_for_signup = "Jaylen Blackwell"
+        self.phone_for_signup = "0999999999"
+        self.postcode_for_signup = "03190"
+        self.address_for_signup = "вул. Миколи Василенка"
 
     def test_get_main_page(self, browser):
         page = BasePage(browser, sets.PROD_SERVER)
@@ -28,7 +32,8 @@ class TestSignupLoginLogautPage:
         page.click_signup()
         page.explicit_wait(2)
         page.is_h1_signup()
-        page.input_email_password(self.email_for_signup, self.password_for_signup)
+        page.input_email_password(self.email_for_signup, self.password_for_signup, self.name_for_signup,
+                                  self.phone_for_signup, self.postcode_for_signup, self.address_for_signup)
         page.press_button_signup()
         page.is_alert_success()
 
@@ -37,8 +42,7 @@ class TestSignupLoginLogautPage:
         page = SignupLoginPage(browser, self.link_to_cabinet)
         page.click_button_signup_login()
         page.explicit_wait(2)
-        page.is_h1_vhod()
-        page.input_email_password(sets.TEST_EMAIL, sets.PASSWORD)
+        page.input_email_password(sets.TEST_EMAIL, sets.PASSWORD, sets.NAME, sets.PHONE, sets.POSTCODE, sets.ADDRESS)
         page.press_button_login()
         page.is_alert_success()
         page.is_button_logout_in_header()
@@ -49,8 +53,4 @@ class TestSignupLoginLogautPage:
         page.press_button_logout()
         page.explicit_wait(2)
         page = MainPage(browser, self.link_to_cabinet)
-        page.is_button_login()
-
-
-
-
+        page.is_button_user()
