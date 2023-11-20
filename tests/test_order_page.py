d3e27bbd0c5db76ad41ecf54994182a8eaa7ffd9
AnastasiiaOkhremchukQA26 <nastia.o0403@gmail.com>
2023-11-20 18:35:00 +0000

    add sets

--- tests/test_order_page.py
+++ tests/test_order_page.py
@@ -3,13 +3,20 @@ from ..pages.base_page import BasePage
 from ..pages.order_page import OrderPage
 from ..pages.signup_login_page import SignupLoginPage
 from ..settings import sets
+import random
 
 @pytest.mark.smoke
 @pytest.mark.regression
 @pytest.mark.order_page
 class TestOrderPage:
     def setup_method(self):
-        pass
+        hash_name = "%032x" % random.getrandbits(128)
+        self.email_for_signup = f"{hash_name}@mail.com"
+        self.password_for_signup = "123654789"
+        self.name_for_signup = "Jaylen Blackwell"
+        self.phone_for_signup = "0999999999"
+        self.postcode_for_signup = "03190"
+        self.address_for_signup = "вул. Миколи Василенка"
 
     def test_get_main_page(self, browser):
         page = BasePage(browser, sets.PROD_SERVER)
@@ -44,6 +51,7 @@ class TestOrderPage:
         global price_2_product
         price_2_product = page.add_to_cart_second_product()
 
+    '''
     def test_check_total_price_qty(self, browser):
         self.link_to_cabinet = browser.current_url
         page = OrderPage(browser, self.link_to_cabinet)
@@ -51,9 +59,13 @@ class TestOrderPage:
         page.explicit_wait(2)
         page.check_total_price_qty(price_1_product, price_2_product, qty=3)  # qty - Quantity - кількість
 
+    '''
     def test_checkout(self, browser):
         self.link_to_cabinet = browser.current_url
         page = OrderPage(browser, self.link_to_cabinet)
+        page.check_products()
+        page.check_cart()
+        page.press_btn_checkout_popup()
         page.press_payment_after_delivery()
         page.add_notice()
         page.press_green_btn_checkout()
